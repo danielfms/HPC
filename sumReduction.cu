e74e15fe06887199a76fc1747c34be8f27bd8c08
danielfms <danielfms3215@yahoo.com>
2015-04-06 17:37:28 -0500

    Funciona!, llenando de ceros.

--- sumReduction.cu
+++ sumReduction.cu
@@ -2,24 +2,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define BLOCK_SIZE 32
+#define BLOCK_SIZE 4
 
 __global__ void reduce(int *g_idata, int *g_odata) {
 	__shared__ int sdata[BLOCK_SIZE];
-	// each thread loads one element from global to shared mem
-	unsigned int tid = threadIdx.x;
-	unsigned int i = blockIdx.x*blockDim.x + threadIdx.x;
-	sdata[tid] = g_idata[i];
-	__syncthreads();
-	// do reduction in shared mem
-	for(unsigned int s=1; s < blockDim.x; s *= 2) {
-		if (tid % (2*s) == 0) {
-		sdata[tid] += sdata[tid + s];
-		}
-	__syncthreads();
-	}
-	// write result for this block to global mem
-	if (tid == 0) g_odata[blockIdx.x] = sdata[0];
+    // each thread loads one element from global to shared mem
+    unsigned int tid = threadIdx.x;
+    unsigned int i = blockIdx.x*blockDim.x + threadIdx.x;
+    sdata[tid] = g_idata[i];
+    __syncthreads();
+    // do reduction in shared mem
+    for(unsigned int s=blockDim.x/2; s > 0; s >>= 1) {
+        if(tid < s){
+            sdata[tid] += sdata[tid + s];
+        }
+        __syncthreads();
+    }
+    // write result for this block to global mem
+    if (tid == 0) g_odata[blockIdx.x] = sdata[0];
 }
 
 
@@ -36,30 +36,34 @@ void llenar(int *A,int N,int a){
         A[i] = a;
 }
 
+void llenar2(int *A,int N,int a){
+	for(int  i = a; i <N; i++ )
+        A[i] = 0;
+}
+
 void imprimir(int *A,int N){
 	for(int i = 0; i <N; i++)
         printf("%d ",A[i]);
-  
 	printf("\n");
 }
 
 
 
 int main(){
-  int N=1024; //2*10240;
-	int s;
+  int N=19; //2*10240;
+  int s;
   int bytes=(N)*sizeof(int);
-	int *A=(int*)malloc(bytes);
+  int *A=(int*)malloc(bytes);
   int *R=(int*)malloc(bytes);
 
 	
   //Lleno las matrices.
-	llenar(A,N,1);
-	llenar(R,N,0);
+  llenar(A,N,1);
+  llenar(R,N,0);
  
   
   //////////////////Algoritmo secuencial///////////////////////
-	clock_t start = clock();      
+  clock_t start = clock();      
   sumS(A,N,&s);
   clock_t end= clock(); 
 	double elapsed_seconds=end-start;
@@ -71,59 +75,60 @@ int main(){
   
   
   //////Separo memoria para el algoritmo paralelo
-	int *d_A=(int*)malloc(bytes);
+  int *d_A=(int*)malloc(bytes);
   int *d_R=(int*)malloc(bytes);
   //cudaMalloc(&d_A,bytes);
   //cudaMalloc(&d_R,bytes);
   cudaMalloc((void**)&d_A,bytes);
   cudaMalloc((void**)&d_R,bytes);
-
   cudaMemcpy(d_A, A, bytes, cudaMemcpyHostToDevice);
   cudaMemcpy(d_R, R, bytes, cudaMemcpyHostToDevice);
-  
   float blocksize=BLOCK_SIZE;
-	dim3 dimGrid(ceil(N/blocksize),1,1);
-  dim3 dimBlock(BLOCK_SIZE,1,1);
-	
-	clock_t start2 = clock();  
-	reduce<<<dimGrid,dimBlock>>>(d_A,d_R);
-	cudaDeviceSynchronize();
-  cudaMemcpy(R, d_R, bytes, cudaMemcpyDeviceToHost);
+  clock_t start2 = clock();  
+  int i=N;
+  while(i>1){
   
+    dim3 dimBlock(BLOCK_SIZE,1,1);
+    int grid=ceil(i/blocksize);
+    dim3 dimGrid(grid,1,1);
+    
+    reduce<<<dimGrid,dimBlock>>>(d_A,d_R);
+    cudaDeviceSynchronize();
+    cudaMemcpy(R, d_R, bytes, cudaMemcpyDeviceToHost);
+    //imprimir(R,N);
+    llenar2(R,N,grid); //Lleno de ceros lo que no tendre en cuenta.
+    cudaMemcpy(d_A,R, bytes, cudaMemcpyHostToDevice);
+    i=ceil(i/blocksize);
+    std::cout<<i<<std::endl;
+  }
+  cudaMemcpy(R, d_R, bytes, cudaMemcpyDeviceToHost);
+
   
   /////////////////////Llamado multikernel///////////////////////
-  for(int i=1;i<N;i*=BLOCK_SIZE){
+ /*for(int i=1;i<N;i*=BLOCK_SIZE){
     reduce<<<dimGrid,dimBlock>>>(d_R,d_A);
   	cudaDeviceSynchronize();
   	cudaMemcpy(d_R, d_A, bytes, cudaMemcpyDeviceToHost);  
-  }
+  } */
   ////////////////////Llamado multikernel///////////////////////
-	cudaMemcpy(R, d_A, bytes, cudaMemcpyDeviceToHost);
 
 
   
   // Copy array back to host
-  
-  
  
   clock_t end2= clock(); 
-	double elapsed_seconds2=end2-start2;
+  double elapsed_seconds2=end2-start2;
   printf("Tiempo transcurrido Paralelo Reduccion: %lf\n", (elapsed_seconds2 / CLOCKS_PER_SEC));  
 	
   /////////////////////////////////////////////////////////////
-  
-	if(s==R[0])
+  if(s==R[0])
     printf("Las sumatorias son iguales: %d %d \n",s,R[0]);
   else
-		printf("Las sumatorias no son iguales: %d %d \n",s,R[0]);
-	
-  for(int i=0;i<N;i++)
-    printf("%d ",R[i]);
+    printf("Las sumatorias no son iguales: %d %d \n",s,R[0]);
   
   free(A);
   free(R);
-	cudaFree(d_A);
-	cudaFree(d_R);
-   
-	return 0;
+  cudaFree(d_A);
+  cudaFree(d_R);
+  return 0;
 }
