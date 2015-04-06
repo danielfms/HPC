6685681ded087ea0e96f9018d674a7b45f2ad290
danielfms <danielfms3215@yahoo.com>
2015-04-06 10:54:20 -0500

    sumReduction con cudaDiviceSynchronize

--- sumReduction.cu
+++ sumReduction.cu
@@ -4,7 +4,7 @@
 
 #define BLOCK_SIZE 32
 
-__global__ void reduce0(int *g_idata, int *g_odata) {
+__global__ void reduce(int *g_idata, int *g_odata) {
 	extern __shared__ int sdata[];
 	// each thread loads one element from global to shared mem
 	unsigned int tid = threadIdx.x;
@@ -24,10 +24,10 @@ __global__ void reduce0(int *g_idata, int *g_odata) {
 
 
 void sumS(int *A,int N ,int *r){
-	int value=0;
+  int value=0;
 	for(int i=0;i<N;i++)
-		value+=A[i];
-	*r=value;
+			value+=A[i];
+  *r=value;
   
 }
 
@@ -46,57 +46,66 @@ void imprimir(int *A,int N){
 
 
 int main(){
-	int N=20;
-	size_t bytes=(N)*sizeof(int);
-	int *A=(int*)malloc(bytes);
-    int *R=(int*)malloc(bytes);
+  int N=4194304;
 	int s;
+  int bytes=(N)*sizeof(int);
+	int *A=(int*)malloc(bytes);
+  int *R=(int*)malloc(bytes);
+
 	
-    //Lleno las matrices.
+  //Lleno las matrices.
 	llenar(A,N,1);
 	llenar(R,N,0);
  
   
-    //////////////////Algoritmo secuencial///////////////////////
+  //////////////////Algoritmo secuencial///////////////////////
 	clock_t start = clock();      
-    sumS(A,N,&s);
-    clock_t end= clock(); 
+  sumS(A,N,&s);
+  clock_t end= clock(); 
 	double elapsed_seconds=end-start;
-    printf("Tiempo transcurrido Secuencial: %lf\n", (elapsed_seconds / CLOCKS_PER_SEC));
-    /////////////////////////////////////////////////////////////
+  printf("Tiempo transcurrido Secuencial: %lf\n", (elapsed_seconds / CLOCKS_PER_SEC));
+  /////////////////////////////////////////////////////////////
   
   
-    ////////////////////////Algoritmo Paralelo///////////////////
+  ////////////////////////Algoritmo Paralelo///////////////////
   
-    //////Separo memoria para el algoritmo paralelo
-	int *d_A;
-    int *d_R;
-    cudaMalloc(&d_A,bytes);
-    cudaMalloc(&d_R,bytes);
+  
+  //////Separo memoria para el algoritmo paralelo
+	int *d_A=(int*)malloc(bytes);
+  int *d_R=(int*)malloc(bytes);
+  //cudaMalloc(&d_A,bytes);
+  //cudaMalloc(&d_R,bytes);
+  cudaMalloc((void**)&d_A,bytes);
+  cudaMalloc((void**)&d_R,bytes);
 
-    cudaMemcpy(d_A, A, bytes, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_R, R, bytes, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_A, A, bytes, cudaMemcpyHostToDevice);
+  cudaMemcpy(d_R, R, bytes, cudaMemcpyHostToDevice);
   
-    float blocksize=BLOCK_SIZE;
+  float blocksize=BLOCK_SIZE;
 	dim3 dimGrid(ceil(N/blocksize),1,1);
-    dim3 dimBlock(blocksize,1,1);
+  dim3 dimBlock(BLOCK_SIZE,1,1);
 	
 	clock_t start2 = clock();  
-	reduce0<<<dimGrid,dimBlock>>>(d_A,d_R);
+	reduce<<<dimGrid,dimBlock>>>(d_A,d_R);
 	cudaDeviceSynchronize();
-    // Copy array back to host
-    cudaMemcpy(R,d_R, bytes, cudaMemcpyDeviceToHost );
-    clock_t end2= clock(); 
+  // Copy array back to host
+  cudaMemcpy(R,d_R, bytes, cudaMemcpyDeviceToHost );
+  clock_t end2= clock(); 
 	double elapsed_seconds2=end2-start2;
-    printf("Tiempo transcurrido Paralelo Reduccion: %lf\n", (elapsed_seconds2 / CLOCKS_PER_SEC));  
+  printf("Tiempo transcurrido Paralelo Reduccion: %lf\n", (elapsed_seconds2 / CLOCKS_PER_SEC));  
 	
-    /////////////////////////////////////////////////////////////
+  /////////////////////////////////////////////////////////////
   
 	if(s!=R[0])
 		printf("Las sumatorias no son iguales: %d %d \n",s,R[0]);
 	
-    for(int i=0;i<N;i++)
-   		printf("%d ",R[i]);
+  //for(int i=0;i<N;i++)
+    //printf("%d ",R[i]);
+  
+  free(A);
+  free(R);
+	cudaFree(d_A);
+	cudaFree(d_R);
    
 	return 0;
 }
