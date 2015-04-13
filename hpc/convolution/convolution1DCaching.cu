f5e738fa893e3115d418895a38dbfc37979a7f29
danielfms <danielfms3215@yahoo.com>
2015-04-12 23:34:35 -0500

    Caching

--- hpc/convolution/convolution1DCaching.cu
+++ hpc/convolution/convolution1DCaching.cu
@@ -3,11 +3,14 @@
 #include <stdlib.h>
 
 #define BLOCK_SIZE 32
+#define MAX_MASK_WIDTH 5
+__constant__ int M[MAX_MASK_WIDTH];
+
 using namespace std;
 
-__global__ void KernelConvolutionBasic(int *N,int *M,int *P,int Mask_Width,int Width){
-	int i = blockIdx.x*blockDim.x + threadIdx.x;
-	int N_start_point = i - (Mask_Width/2);
+__global__ void KernelConvolutionCaching(int *N,int *P,int Mask_Width,int Width){
+  int i = blockIdx.x*blockDim.x + threadIdx.x;
+  int N_start_point = i - (Mask_Width/2);
     int Pvalue=0;
     for (int j= 0;j<Mask_Width;j++) {
       if (N_start_point+j >= 0 && N_start_point + j < Width) {
@@ -51,10 +54,10 @@ void llenar(int *V,int N,int flag){
 void compare(int*A,int *B,int width){
   for(int i=0;i<width;i++)
     if(A[i]!=B[i]){
-    	cout<<"Los vectores no son iguales"<<endl;
-    	return;
-  	}
-  cout<<"Los vectores son iguales"<<endl;	 
+      cout<<"Los vectores no son iguales"<<endl;
+      return;
+    }
+  cout<<"Los vectores son iguales"<<endl;  
 }
 
 int main(){
@@ -63,7 +66,7 @@ int main(){
   int bytes=(N)*sizeof(int);
   int *V=(int*)malloc(bytes);
   int *P=(int*)malloc(bytes);
-  int Mask[5]={3,4,5,4,3};
+  int Mask[MAX_MASK_WIDTH]={3,4,5,4,3};
   
   llenar(V,N,1);
   llenar(P,N,0);
@@ -81,19 +84,21 @@ int main(){
   //Variables para el kernel
   int *d_V;
   int *d_P;
-  int *d_Mask;
-  int bytesM=5*sizeof(int);
   int *P_out=(int*)malloc(bytes);
   int *P_in=(int*)malloc(bytes);
+
+  //Constant Memory
+  int bytesM=MAX_MASK_WIDTH*sizeof(int);
+  cudaMemcpyToSymbol(M,Mask,bytesM);
+
   llenar(P_in,N,0);
   
   cudaMalloc(&d_V,bytes);
   cudaMalloc(&d_P,bytes);
-  cudaMalloc(&d_Mask,bytesM);
 
   cudaMemcpy(d_V, V, bytes, cudaMemcpyHostToDevice);
   cudaMemcpy(d_P, P_in, bytes, cudaMemcpyHostToDevice);
-  cudaMemcpy(d_Mask, Mask, bytesM, cudaMemcpyHostToDevice);
+
   
   //Bloque y grid
   float blocksize=BLOCK_SIZE;
@@ -102,7 +107,8 @@ int main(){
   
   //Convolucion Paralelo
   start=clock();
-  KernelConvolutionBasic<<<dimGrid,dimBlock>>>(d_V,d_Mask,d_P,5,N);
+  int mask_width=MAX_MASK_WIDTH;
+  KernelConvolutionCaching<<<dimGrid,dimBlock>>>(d_V,d_P,mask_width,N);
   cudaDeviceSynchronize();
   cudaMemcpy(P_out,d_P, bytes, cudaMemcpyDeviceToHost );
   end=clock();
@@ -113,6 +119,5 @@ int main(){
   
   compare(P,P_out,N);
   cout<<"Aceleracion obtenida: "<<elapsed_seconds/elapsed_seconds2<<endl;
-  
   return 0; 
 }
